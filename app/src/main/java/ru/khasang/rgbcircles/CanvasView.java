4f4fc94adb7366f5530aac27cc0361e288df2bd0
Anastasiya_Moon <alleksa_anastasiya@mail.ru>
2020-02-04 00:01:10 +0300

    определение размера экрана и размещение круга посередине

--- app/src/main/java/ru/khasang/rgbcircles/CanvasView.java
+++ app/src/main/java/ru/khasang/rgbcircles/CanvasView.java
@@ -4,18 +4,41 @@ package ru.khasang.rgbcircles;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
+import android.graphics.Point;
 import android.util.AttributeSet;
+import android.view.Display;
 import android.view.View;
+import android.view.WindowManager;
 import androidx.annotation.Nullable;
 
 public class CanvasView extends View {
+    //ширина и высота экрана
+    private static int width;
+    private static int height;
     private GameManager gameManager;
 
+    //конструктор CanvasView
     public CanvasView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        //выделяем память, проинициализировав
-        gameManager = new GameManager();
+        //определяем width, height через метод
+        initWidthAndHeight(context);
 
+        //выделяем память, проинициализировав
+        //передаем ссылку на себя this (CanvasView)
+        gameManager = new GameManager(this, width, height);
+
+    }
+
+    private void initWidthAndHeight(Context context) {
+        //узнаем ширину / высоту окна
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        //получаем дисплей
+        Display display = windowManager.getDefaultDisplay();
+        Point point = new Point();
+        display.getSize(point);
+        //point правая нижняя точка экрана и из нее получаем x|y
+        width = point.x;
+        height = point.y;
     }
 
     @Override
